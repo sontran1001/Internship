020499ab954a5bf93fecbb781dc7b7ff02bf3020
Son Tran <sontn.ac@gmail.com>
2016-12-05 14:33:26 +0100

    Remove duplicated directory

--- src/bim_extflash/FlashOnly_ST/TOOLS/subdir_vars.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
-CMD_SRCS += \
-C:/ti/simplelink/ble_sdk_2_02_01_18/src/common/cc26xx/ccs/cc26xx_bim_ext_flash.cmd 
-
-
